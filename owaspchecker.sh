00bfff8554c59a5262488fd8f68e19f1fc98963d
CyberSecTmken <52453975+CyberSecTmken@users.noreply.github.com>
2020-06-17 15:17:41 -0700

    Update owaspchecker.sh

--- owaspchecker.sh
+++ owaspchecker.sh
@@ -5,7 +5,9 @@ DC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DC_PROJECT="dependency-check scan: $(pwd)"
 DATA_DIRECTORY="$DC_DIRECTORY/data"
 CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
-
+echo "$DATA_DIRECTORY"
+echo "$CACHE_DIRECTORY"
+echo "aldeheme"
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
     mkdir -p "$DATA_DIRECTORY"
