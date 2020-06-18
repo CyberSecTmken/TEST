9397feb101a4181e0b4ab74217de585c4d946480
CyberSecTmken <52453975+CyberSecTmken@users.noreply.github.com>
2020-06-18 04:19:23 -0700

    Update owaspchecker.sh

--- owaspchecker.sh
+++ owaspchecker.sh
@@ -26,7 +26,7 @@ docker run --rm \
     --volume $(pwd)/odc-reports:/report:z \
     owasp/dependency-check:$DC_VERSION \
     --scan /src \
-    --format "ALL" \
+    --format "XML" \
     --project "$DC_PROJECT" \
     --out /report
     # Use suppression like this: (where /src == $pwd)
